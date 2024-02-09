5e3596c8b6c366f9efd2920b3e5ac504e93d6881
Yves <yves.habchy@gmail.com>
2024-02-09 20:59:31 +0200

    Set automatic scroll down for message display

--- src/app/components/MessageDisplay.tsx
+++ src/app/components/MessageDisplay.tsx
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
 import humanLogo from "../icons/human.png";
 import aiLogo from "../icons/ai.png";
+import { useRef } from "react";
 interface MessageDisplayProps {
   messages?: string[];
 }
 
 const MessageDisplay = ({ messages }: MessageDisplayProps) => {
+  const chatContainerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (chatContainerRef.current) {
+      chatContainerRef.current.scrollIntoView({
+        behavior: "smooth",
+        block: "end",
+      });
+    }
+  }, [messages]);
   return (
-    <div className="w-100 mt-4 h-80 overflow-auto break-all bg-gray-100 p-4 text-lg">
+    <div
+      className="w-100 mt-4 h-80 overflow-auto break-all bg-gray-100 p-4 text-lg"
+      ref={chatContainerRef}
+    >
       {messages?.map((message, index) => (
-        <div key={index} className="mb-3 flex gap-1">
+        <div key={index} ref={chatContainerRef} className="mb-3 flex gap-1">
           {index % 2 === 0 ? (
             <Image
               alt="User"
